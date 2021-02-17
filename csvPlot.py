a0b4dc9777e0b831cdcc0e5c65e9f7eafe09e8e6
star-fruit <52576397+star-fruit@users.noreply.github.com>
2021-02-17 10:01:17 -0500

    Update Plotting Function

--- csvPlot.py
+++ csvPlot.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import time
-#Finished Jan 6th 2020
+from matplotlib import dates
+
 def plotData(path, quantity):
 
     df = pd.read_csv(path)
 
-    timeArray = [time( int(timestring[7:9]), int(timestring[9:11]), second = int(timestring[11:13])) for timestring in [str(df.loc[index, 'DDMMYYYYHHMMSS']) for index, row in df.iterrows()]]
-   
+    timeArray = [ time( int(timestring[8:10]), int(timestring[10:12]), second = int(timestring[12:14]) ) 
+                 for timestring in [str(df.loc[index, 'DDMMYYYYHHMMSS']) for index, row in df.iterrows()] ]
+    
     timeArray = pd.to_datetime(timeArray, format="%H:%M:%S")#Converting into pandas time format (with standard year, month, day)
-    plt.xticks(rotation=30)
     
     plt.plot(timeArray, df[quantity])
+    plt.gcf().autofmt_xdate()
+    date_format= dates.DateFormatter('%H:%M:%S')
+    plt.gca().xaxis.set_major_formatter(date_format)
     plt.title(f'{quantity} vs Time')
     plt.xlabel('Time')
     plt.ylabel(f'{quantity}')
     plt.show()
+    
     
